818f94dd3245437c2bbf4e58b3a97ff648c484ff
Joshua Marantz <jmarantz@gmail.com>
2019-01-12 10:33:41 -0500

    board construction, randomization and serialization

--- life.py
+++ life.py
@@ -3,6 +3,7 @@
 import argparse
 import csv
 import http.server
+import random
 import socketserver
 import sys
 import urllib.parse
@@ -35,23 +36,40 @@ class myHandler(http.server.BaseHTTPRequestHandler):
             height = int(q['height'][0]) if 'height' in q else width
             density = float(q['density'][0]) if 'density' in q else 0.1
             print("width=%d height=%d density=%f" % (width, height, density))
+            board = Board(width, height, density)
+            self.wfile.write(board.serialize().encode('ascii'))
         else:
-            self.wfile.write(b'Hello, world!\n')
+            self.wfile.write(b'Commands:\n  /quit\n  /board?width=x&height=y&density=d\n')
 
 class Board:
     """Represents a board in the Conway's Game of Life"""
 
-    def __init__(self, width, height):
+    def __init__(self, width, height, density):
         self.width = width
         self.height = height
         self.rows = []
         for r in range(height):
             row = []
             for c in range(width):
-                row.append(False)
+                row.append(random.randint(0, 1000) < 1000 * density)
             self.rows.append(row)
 
+    def serialize(self):
+        out = ""
+        for h in range(self.height):
+            # Densities in life are usually low, just print deltas of True
+            row = self.rows[h]
+            prev_true = -1
+            true_deltas = []
+            for c in range(self.width):
+                if row[c]:
+                    true_deltas.append(c - prev_true)
+                    prev_true = c
+            out = out + ("%s" % true_deltas) + "\n"
+        return out
+
 def main(argv):
+    random.seed(32)
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", help="http port", type=int, default=100)
     parser.add_argument("--width", help="width", type=int, default=100)
